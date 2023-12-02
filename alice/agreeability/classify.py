759f006db5c340f9dabef75a0d501329d65e9e20
Bachana Anasashvili <130998713+anasashb@users.noreply.github.com>
2023-12-02 16:12:37 +0100

    added zero handling

--- alice/agreeability/classify.py
+++ alice/agreeability/classify.py
@@ -11,9 +11,11 @@ def cohen_kappa(x, y):
     d = np.sum((x == 0) & (y == 0))
     # Total (reusable)
     total = a + b + c + d
+    if total == 0:
+        return 0
     # Observed proportionate agreement
     p_o = (a + d) / total
     # Overall random agreement probability (p_yes + p_no)
-    p_e = (((a + b) * (a + c)) + ((c + d) * (b + d))) / total ** 2
+    p_e = ((a + b) * (a + c) + (c + d) * (b + d)) / total ** 2
     # Cohen's kappa
     return (p_o - p_e) / (1 - p_e) if (1 - p_e) !=0 else 0
